378217f56a2d5f51bdbbaa88f69794fed3002292
Saleem Abdullah <saleembozdar1@gmail.com>
2024-02-24 12:39:06 +0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,106 @@
+"use client";
 import Image from "next/image";
+import { userInfo } from "os";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [data, setData] = useState([]);
+  const [dataFilter, setFilter] = useState([]);
+  const [searchText, setSearchText] = useState("");
+
+  function fecthUser(userid: string) {
+    return new Promise((resolve, reject) => {
+      fetch(`https://jsonplaceholder.typicode.com/users/${userid}`)
+        .then((data: Response) => {
+          console.log(
+            data.json().then((jsonData) => {
+              resolve(jsonData);
+            })
+          );
+        })
+        .catch(reject);
+    });
+  }
+
+  async function fetchPosts() {
+    fetch("https://jsonplaceholder.typicode.com/posts").then(
+      async (data: Response) => {
+        console.log(
+          data.json().then(async (jsonData) => {
+            for (var index = 0; index <= jsonData.length - 1; index++) {
+              var value = jsonData[index];
+              const user = await fecthUser(value?.userId);
+              value.userInfo = user;
+              jsonData[index] = value;
+              console.log("setting json");
+            }
+            setData(jsonData);
+          })
+        );
+      }
+    );
+  }
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  function filterByTitle(data, text) {
+    return data?.filter((filter) => filter?.title === text);
+  }
+
+  useEffect(() => {
+    if (searchText === "") setFilter(data);
+    else setFilter(filterByTitle(data, searchText));
+  }, [data, searchText]);
+
+  console.log(data);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main className="">
+      <div>
+        <center>
+          Search By Title
+          <input
+            style={{ border: "1px solid black", color: "black" }}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+        </center>
+        <table style={{ width: "100%" }}>
+          <tr>
+            <th style={{ width: 1 }}>USER ID</th>
+            <th style={{ width: 1 }}>POST ID</th>
+            <th style={{ width: 1 }}>TITLE</th>
+            <th style={{ width: 1 }}>Body</th>
+            <th style={{ width: 1 }}>Name</th>
+            <th style={{ width: 1 }}>Username</th>
+            <th style={{ width: 1 }}>Email</th>
+            <th style={{ width: 1 }}>Address</th>
+            <th style={{ width: 1 }}>Phone</th>
+            <th style={{ width: 1 }}>Website</th>
+            <th style={{ width: 1 }}>Company</th>
+            <th style={{ width: 1 }}>Edit</th>
+          </tr>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          {dataFilter?.map((value, index) => {
+            return (
+              <tr>
+                <th style={{ width: 1 }}>{value?.userId}</th>
+                <th style={{ width: 1 }}>{value?.id}</th>
+                <th style={{ width: 1 }}>{value?.title}</th>
+                <th style={{ width: 1 }}>{value?.body}</th>
+                <th style={{ width: 1 }}>{value?.userInfo?.name}</th>
+                <th style={{ width: 1 }}>{value?.userInfo?.username}</th>
+                <th style={{ width: 1 }}>{value?.userInfo?.email}</th>
+                <th style={{ width: 1 }}>{value?.userInfo?.address?.street}</th>
+                <th style={{ width: 1 }}>{value?.userInfo?.phone}</th>
+                <th style={{ width: 1 }}>{value?.userInfo?.website}</th>
+                <th style={{ width: 1 }}>{value?.userInfo?.company?.name}</th>
+                <th style={{ width: 1 }}>Edit</th>
+              </tr>
+            );
+          })}
+        </table>
       </div>
     </main>
   );
